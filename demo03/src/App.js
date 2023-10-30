f9f81a500716f4616bfcf3a9acd3e8c4e72edabd
yejishin95 <yeji2336@naver.com>
2023-10-30 16:46:17 +0900

    첫번째 예제 불변수로 더하기빼기

---
+++ demo03/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Exam01 from './components/Exam01';
+
+function App() {
+  return (
+<>
+<h1>각종 리액트 예제</h1>
+<hr/>
+<Exam01/>
+</>  
+  
+  );
+}
+
+export default App;
---
+++ demo03/src/components/Exam02.js
@@ -0,0 +1,11 @@
+function Exam02() {
+return (
+    <>
+        <h1>두 번째 예제</h1>
+    </>
+);
+
+}
+
+
+export default Exam02;
